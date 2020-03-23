c796eca4f4513952a329546f3bcaaac8a28f3d97
Gabesz Rozsa <gabesz.rozsa@gmail.com>
2020-03-23 06:17:34 +0100

    final :rocket:

---
+++ src/app.module.ts
@@ -0,0 +1,26 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { PetsController } from './pets/pets.controller';
+import { Pet } from './pets/pet.entity';
+
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([Pet]),
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      url: process.env.MONGODB_CONNECTION_STRING,
+      database: process.env.MONGODB_DATABASE,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      ssl: true,
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    }),
+  ],
+  controllers: [AppController, PetsController],
+  providers: [AppService],
+})
+export class AppModule {}
